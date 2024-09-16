c65c9c2e497820ff50c7afe21d728bb6f51f9a36
pablolospe <yosoypxl@gmail.com>
2024-09-16 02:10:19 -0300

    second commit

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET(request: Request) { 
+
+  return new Response(JSON.stringify({
+    hola: 'Mund@!!!!'
+  }), { status: 200 } );
+}
+export async function POST(request: Request) { 
+
+  return new Response(JSON.stringify({
+    hola: 'Mund@!!!!',
+    method: "POST"
+  }), { status: 200 } );
+}
