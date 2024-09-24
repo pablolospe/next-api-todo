974c86702b7f94bbcdc3928f9c1a48b67326c966
pablolospe <yosoypxl@gmail.com>
2024-09-24 15:37:26 -0300

    toggle en serveraction!

---
+++ src/todos/actions/todo-actions.ts
@@ -0,0 +1,23 @@
+'use server'
+
+import prisma from "@/app/lib/prisma"
+import { Todo } from "@prisma/client"
+import { revalidatePath } from "next/cache"
+
+
+
+export const toggleTodo = async (id: string, complete: boolean):Promise<Todo> => {
+    const todo = await prisma.todo.findFirst({ where: { id } })
+
+    if (!todo) {
+        throw `Todo con id ${id} no encontrado`
+    }
+
+    const updatedTodo = await prisma.todo.update({
+        where: {id},
+        data: {complete}
+    })
+
+    revalidatePath('/dashboard/server-todos')
+    return updatedTodo
+}
