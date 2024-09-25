5e9df00f2596e34985b3b7747451986c0a47ccfb
pablolospe <yosoypxl@gmail.com>
2024-09-25 16:45:09 -0300

    sleep() comentado

--- src/todos/actions/todo-actions.ts
+++ src/todos/actions/todo-actions.ts
@@ -17,7 +17,7 @@ export const sleep = async ( seconds: number = 0) => {
 
 export const toggleTodo = async (id: string, complete: boolean): Promise<Todo> => {
 
-    await sleep(3);
+    // await sleep(3);
 
     const todo = await prisma.todo.findFirst({ where: { id } });
 
